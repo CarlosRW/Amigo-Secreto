f72d218fa5042318740b8392e9c7b3e44399aa02
Carlos / <153391284+CarlosRW@users.noreply.github.com>
2025-01-24 01:29:55 -0600

    Implement amigo addition functionality and changed img folder to assets

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Varibales
+let amigos = [];
 
-let amigos = []
+// Funciones
+function agregarAmigo() {
+    let amigo = document.getElementById('amigo').value;
+    if (amigo !== '') {
+        amigos.push(amigo);
+        document.getElementById('amigo').value = '';
+        console.log(amigos);
+    } else {
+        alert('Por favor, inserte un nombre.');
+    }
+}
